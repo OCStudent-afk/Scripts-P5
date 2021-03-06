5b8f6ca187e5d288806334064f80f326ad647603
OCStudent-afk <80049280+OCStudent-afk@users.noreply.github.com>
2021-03-06 03:26:48 +0100

    Update Projet05_Valdin_Script03.ps1
    Added exit codes to the scripts

--- Projet05_Valdin_Script03.ps1
+++ Projet05_Valdin_Script03.ps1
@@ -2,9 +2,9 @@
 # Script listant et exportant les groupes dont un utilisateur est membre en fichier .txt
 # AUTHEUR: Valdin Laurie, ACME Group
 # DATE: 01/03/2021
-# VERSION 1.0
+# VERSION 1.1
 
-$utilisateur = Read-Host "Merci de rentrer le prénom de l’utilisateur en question"
+$utilisateur = Read-Host "Merci de rentrer le prÃ©nom de lâ€™utilisateur en question"
 
 $user = Get-AdUser -Identity $utilisateur -Properties MemberOf
 
@@ -20,4 +20,6 @@ foreach($u in $user) {
 
 }
 
-Write-Host "Liste des groupes dont l'utilisateur fait partie récupérée avec succès"
+Write-Host "Liste des groupes dont l'utilisateur fait partie rÃ©cupÃ©rÃ©e avec succÃ¨s"
+
+exit 0
